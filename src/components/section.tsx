0b8d5b33f4e82dafdebbfc166dc68e7dc5e905f3
JNickBaughan <JNickBaughan@gmail.com>
2020-09-19 17:57:30 -0400

    add section component

---
+++ src/components/section.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+
+const StyledSection = styled.div`
+  margin-top: 60px;
+  margin-bottom: 20px;
+  box-shadow: 4px 4px 2px #888888;
+  padding: 40px;
+`;
+
+export const Section = React.forwardRef((_, ref) => {
+  return (
+    <StyledSection ref={ref}>
+      {" "}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+    </StyledSection>
+  );
+});
+
+export default Section;
